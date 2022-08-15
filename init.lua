c0fb0a328f898e9b904ad447b432cbab51ae979a
FP <fp_crypt@protonmail.com>
2022-08-14 17:55:18 -0700

    fix: transparent theme

--- init.lua
+++ init.lua
@@ -10,10 +10,3 @@ require("user.cmp")
 require("user.treesitter")
 require("user.dap")
 require("user.toggleterm")
-
--- Some vimscript refugees
--- "syntax on
--- "hi Normal guibg=NONE ctermbg=NONE
--- 
--- "set backspace=indent,eol,start
--- "filetype plugin indent
--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,4 +1,5 @@
 vim.g.tokyonight_style = "storm"
 vim.g.tokyonight_transparent_sidebar = true
 vim.g.tokyonight_dark_float = true
+vim.g.tokyonight_transparent = true
 vim.cmd[[colorscheme tokyonight]]
