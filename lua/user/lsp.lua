b9d9395c2bea0887b8abb302eef793a8a1ff6b68
FP <fp_crypt@protonmail.com>
2023-09-07 16:10:41 -0700

    fix: solidity ls

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -1,14 +1,14 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = {
-    "pyright",
-    "solidity_ls",
-    "lua_ls",
-    "vimls",
-    "tsserver",
-    "rust_analyzer",
-    "yamlls",
-  },
+	ensure_installed = {
+		"pyright",
+		"solidity",
+		"lua_ls",
+		"vimls",
+		"tsserver",
+		"rust_analyzer",
+		"yamlls",
+	},
 })
 
 local lspcfg = require("lspconfig")
@@ -16,29 +16,29 @@ local lspcfg = require("lspconfig")
 lspcfg.pyright.setup({})
 lspcfg.solidity_ls.setup({})
 lspcfg.lua_ls.setup({
-  settings = {
-    Lua = {
-      runtime = {
-        version = "LuaJIT",
-      },
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-  on_attach = function(
-  client,
-  _ --[[buffer--]]
-  )
-    client.server_capabilities.document_formatting = false -- don't use semneko formatting
-  end,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+	on_attach = function(
+		client,
+		_ --[[buffer--]]
+	)
+		client.server_capabilities.document_formatting = false -- don't use semneko formatting
+	end,
 })
 lspcfg.vimls.setup({})
 lspcfg.tsserver.setup({})
@@ -51,21 +51,21 @@ local diagnostics = null_ls.builtins.diagnostics
 --local completion = null_ls.builtins.completion
 
 require("null-ls").setup({
-  sources = {
-    formatting.stylua,
-    formatting.prettier.with({
-      extra_filetypes = { "solidity" },
-    }),
-    diagnostics.solhint.with({
-      method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
-    }),
-    formatting.black,
-    diagnostics.hadolint,
-  },
+	sources = {
+		formatting.stylua,
+		formatting.prettier.with({
+			extra_filetypes = { "solidity" },
+		}),
+		diagnostics.solhint.with({
+			method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+		}),
+		formatting.black,
+		diagnostics.hadolint,
+	},
 })
 
 vim.api.nvim_create_user_command("Format", function(opts)
-  vim.lsp.buf.format()
+	vim.lsp.buf.format()
 end, { range = true })
 -- vim.api.nvim_create_user_command("FormatSelection", function(opts)
 --   --vim.pretty_print(opts)
