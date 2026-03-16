424ccef67ad7810519ad9b35f05391f62bd7789b
FP <fp_crypt@protonmail.com>
2026-03-16 13:52:54 -0700

    feat: migrate treesitter config to new API
    Replace old nvim-treesitter.configs setup with the new nvim-treesitter
    .install() API and inline config into lazy.lua, removing the separate
    treesitter.lua file. Drop redundant rainbow-delimiters setup since
    defaults match our config.

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -39,16 +39,19 @@ require("lazy").setup({
 			},
 			event = { "VeryLazy" },
 			lazy = vim.fn.argc(-1) == 0,
-			init = function(plugin)
-				-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-				-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-				-- no longer trigger the **nvim-treesitter** module to be loaded in time.
-				-- Luckily, the only things that those plugins need are the custom queries, which we make available
-				-- during startup.
-				require("lazy.core.loader").add_to_rtp(plugin)
-				require("nvim-treesitter.query_predicates")
-			end,
 			cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+			config = function()
+				require("nvim-treesitter").install({
+					"python",
+					"solidity",
+					"go",
+					"vim",
+					"lua",
+					"yaml",
+					"json",
+					"javascript",
+				})
+			end,
 		},
 
 		-- theme
